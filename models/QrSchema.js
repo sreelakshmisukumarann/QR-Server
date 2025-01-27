69b8e901af591729e1c1670e1f16589a2eb3c680
Sreelakshmi P S <sreelakshmips32@gmail.com>
2025-01-27 10:36:41 +0530

    updated schema

--- models/QrSchema.js
+++ models/QrSchema.js
@@ -5,24 +5,22 @@ const QRSchema = new mongoose.Schema(
   {
     slug: {
       type: String,
-      required: true, 
+      required: true,
     },
     source: {
       type: String,
-      required: true, 
+      required: true,
     },
     ipAddress: {
-       type: String, required: true
-       }, // IP address of the scanner
-    scannedAt: {
-       type: Date, default: Date.now 
-      }, // Timestamp
-  }
-  // { timestamps: true } // Automatically add `createdAt` and `updatedAt` fields
+      type: String,
+      required: true, // IP address of the scanner
+    },
+  },
+  { timestamps: true } // This option is passed outside the field definitions
 );
 
 // Create model
-const ScanLog = mongoose.model("ScanLog", QRSchema); // Updated model name to `ScanLog`
+const ScanLog = mongoose.model("ScanLog", QRSchema);
 
 // Export the model
 module.exports = ScanLog;
